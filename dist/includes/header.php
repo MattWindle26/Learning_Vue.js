e372bdb760c969be5bf5240caf960f492d48792c
matthew windle <matthew windle>
2017-05-13 23:26:29 +0100

    - created a price table in vue

--- dist/includes/header.php
+++ dist/includes/header.php
@@ -2,7 +2,7 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <title>Your title here</title>
+    <title>Learning Vue.js</title>
     <link rel="stylesheet" href="/css/main.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://unpkg.com/vue"></script>
